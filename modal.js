e2828bcf889c7aaee916ae7fd9f0e3590e4b2ace
Meshcheryakov Andrey <mesheryakov97@gmail.com>
2022-01-31 21:05:02 +0500

    $upd: new visual

--- modal.js
+++ modal.js
@@ -1,134 +1,148 @@
-const body = document.querySelector('body');
-const popupLinks = document.querySelectorAll('.popup-link'); // добавить на ссылку,чтобы открыть попап
-const lockPadding = document.querySelectorAll('.lock-padding'); // добавить на фиксированный объект, чтобы не дергался
-
 // TODO button и вместо href="id popup" data-popup="id popup"
-
-let unlock = true;
-
-const timeout = 400; // transition указан в css
-
-// поиск popup
-if (popupLinks.length > 0) {
-  for (let i = 0; i < popupLinks.length; i++) {
-    const popupLink = popupLinks[i]
-    popupLink.addEventListener('click', function (e) {
-      const popupName = popupLink.getAttribute('href').replace('#', '');
-      const currentPopup = document.getElementById(popupName);
-      popupOpen(currentPopup);
-      e.preventDefault();
-    })
+if (window.addEventListener) {
+  window.addEventListener('load', createModal, false);
+} else {
+  if (window.attachEvent) {
+    window.attachEvent('onload', createModal);
+  } else {
+    window.onload = createModal;
   }
 }
 
-// кнопка закрытия
-const popupCloseBtn = document.querySelectorAll('.modal-close');
+function createModal() {
+  const body = document.querySelector('body');
+  const popupLinks = document.querySelectorAll('.js-popup-link'); // добавить на ссылку,чтобы открыть попап
+  const lockPadding = document.querySelectorAll('.lock-padding'); // добавить на фиксированный объект, чтобы не дергался
 
-if (popupCloseBtn.length > 0) {
-  for (let i = 0; i < popupCloseBtn.length; i++) {
-    const el = popupCloseBtn[i];
-    el.addEventListener('click', function (e) {
-      popupClose(el.closest('.modal')); // ближайший родитель кнопки
-      e.preventDefault();
-    });
-  }
-}
+  let unlock = true;
 
-// открытие
-function popupOpen(currentPopup) {
-  if (currentPopup && unlock) {
-    const popupActive = document.querySelector('.modal.active');
-    if (popupActive) {
-      popupClose(popupActive, false); // закрыть popup при открытии нового
-    } else {
-      bodyLock();
+  const timeout = 400; // transition указан в css
+
+  // поиск popup
+  if (popupLinks.length > 0) {
+    for (let i = 0; i < popupLinks.length; i++) {
+      const popupLink = popupLinks[i]
+      popupLink.addEventListener('click', function (e) {
+        const popupName = popupLink.getAttribute('href').replace('#', '');
+        const currentPopup = document.getElementById(popupName);
+        popupOpen(currentPopup);
+        e.preventDefault();
+      })
     }
-    currentPopup.classList.add('active');
-    currentPopup.addEventListener('click', function (e) {
-      if (!e.target.closest('.modal__body > div')) {
-        popupClose(e.target.closest('.modal')); // закрытие по клику на тёмную область
+  }
+
+  // кнопка закрытия
+  const popupCloseBtn = document.querySelectorAll('.modal-close');
+
+  if (popupCloseBtn.length > 0) {
+    for (let i = 0; i < popupCloseBtn.length; i++) {
+      const el = popupCloseBtn[i];
+      el.addEventListener('click', function (e) {
+        popupClose(el.closest('.modal')); // ближайший родитель кнопки
+        e.preventDefault();
+      });
+    }
+  }
+
+  // открытие
+  function popupOpen(currentPopup) {
+    if (currentPopup && unlock) {
+      const popupActive = document.querySelector('.modal.active');
+      if (popupActive) {
+        popupClose(popupActive, false); // закрыть popup при открытии нового
+      } else {
+        bodyLock();
       }
-    });
-  }
-}
-
-function popupClose(popupActive, doUnlock = true) {
-  if (unlock) {
-    popupActive.classList.remove('active');
-    if (doUnlock) {
-      bodyUnLock();
+      currentPopup.classList.add('active');
+      currentPopup.addEventListener('click', function (e) {
+        if (!e.target.closest('.modal__body > div')) {
+          popupClose(e.target.closest('.modal')); // закрытие по клику на тёмную область
+        }
+      });
     }
   }
-}
 
-// фиксация прокрутки
-function bodyLock() {
-  const lockPaddingValue = window.innerWidth - document.querySelector('body').offsetWidth + 'px';
-
-  if (lockPadding.length > 0) {
-    for (let i = 0; i < lockPaddingValue.length; i++) {
-      const el = lockPadding[i];
-      el.style.paddingRight = lockPaddingValue;
+  function popupClose(popupActive, doUnlock = true) {
+    if (unlock) {
+      popupActive.classList.remove('active');
+      if (doUnlock) {
+        bodyUnLock();
+      }
     }
-  };
+  }
 
-  body.style.paddingRight = lockPaddingValue;
-  body.classList.add('lock');
+  // фиксация прокрутки
+  function bodyLock() {
+    const lockPaddingValue = window.innerWidth - document.querySelector('body').offsetWidth + 'px';
 
-  unlock = false;
-  setTimeout(function () {
-    unlock = true;
-  }, timeout)
-}
-
-function bodyUnLock() {
-  setTimeout(function () {
     if (lockPadding.length > 0) {
-      for (let i = 0; i < lockPadding.length; i++) {
+      for (let i = 0; i < lockPaddingValue.length; i++) {
         const el = lockPadding[i];
-        el.style.paddingRight = '0px';
+        el.style.paddingRight = lockPaddingValue;
       }
-    }
-    body.style.paddingRight = '0px';
-    body.classList.remove('lock');
-  }, timeout);
-
-  unlock = false;
-  setTimeout(function () {
-    unlock = true;
-  }, timeout);
-}
-
-// esc
-document.addEventListener('keydown', function (e) {
-  if (e.which === 27) {
-    const popupActive = document.querySelector('.modal.active');
-    popupClose(popupActive);
-  }
-})
-  /*
-// полифилы старые браузеры
-(function() {
-  if (!Element.prototype.closest) {
-
-    Element.prototype.closest = function (css) {
-      var node = this;
-      while (node) {
-        if(node.matches(css)) return node;
-        else node = node.parentElement;
-      }
-      return null;
     };
+
+    body.style.paddingRight = lockPaddingValue;
+    body.classList.add('lock');
+
+    unlock = false;
+    setTimeout(function () {
+      unlock = true;
+    }, timeout)
   }
-}) ();
-(function() {
-  //проверка поддержки
-  if (!Element.prototype.matches) {
-    //определение свойства
-    Element.prototype.matches = Element.prototype.matchesSelector ||
-    Element.prototype.webkitMatchesSelector ||
-    Element.prototype.mozMatchesSelector ||
-    Element.prototype.msMatchesSelector ;
-  };
-}) ();
+
+  function bodyUnLock() {
+    setTimeout(function () {
+      if (lockPadding.length > 0) {
+        for (let i = 0; i < lockPadding.length; i++) {
+          const el = lockPadding[i];
+          el.style.paddingRight = '0px';
+        }
+      }
+      body.style.paddingRight = '0px';
+      body.classList.remove('lock');
+    }, timeout);
+
+    unlock = false;
+    setTimeout(function () {
+      unlock = true;
+    }, timeout);
+  }
+
+  // esc
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape') {
+      const popupActive = document.querySelector('.modal.active');
+      popupClose(popupActive);
+    }
+  })
+
+  createModal.popupOpen = popupOpen
+
+  /*
+  // полифилы старые браузеры
+  (function() {
+    if (!Element.prototype.closest) {
+
+      Element.prototype.closest = function (css) {
+        var node = this;
+        while (node) {
+          if(node.matches(css)) return node;
+          else node = node.parentElement;
+        }
+        return null;
+      };
+    }
+  }) ();
+  (function() {
+    //проверка поддержки
+    if (!Element.prototype.matches) {
+      //определение свойства
+      Element.prototype.matches = Element.prototype.matchesSelector ||
+      Element.prototype.webkitMatchesSelector ||
+      Element.prototype.mozMatchesSelector ||
+      Element.prototype.msMatchesSelector ;
+    };
+  }) ();
 */
+}
